6a35685a133e7da2e0090776cea7abca07662a2f
Lavsexpert <lavrov-sergey@yandex.ru>
2018-12-02 19:57:57 +0300

    Начали работать кнопки Остановить и Сбросить.

--- src/App.js
+++ src/App.js
@@ -8,11 +8,15 @@ class App extends Component {
 	
     constructor(props) {
 		super(props);
-		this.state = {value: 0};
+		this.state = {value: 0, stopped: false};
+		
+		// Привязка необходима, чтобы сделать this доступным в коллбэке
+		this.stopTimer = this.stopTimer.bind(this);
+		this.resetTimer = this.resetTimer.bind(this);
     }
 
     increment(){
-		this.setState({value: this.state.value + 1});
+		if(!this.state.stopped) (this.setState({value: this.state.value + 1}));
     }
 
     componentDidMount() {
@@ -22,9 +26,19 @@ class App extends Component {
     componentWillUnmount() {
 		clearInterval(this.timerID);
     }
+	
+	stopTimer(){
+		clearInterval(this.timerID);
+	}
+
+	resetTimer(){
+		this.setState({value: 0});		
+	}
 
     render() {
-		const value = this.state.value
+		const value = this.state.value;
+		document.title = "Таймер: "+Math.floor(value/INTERVAL/60/60)+":"
+		+Math.round(value/INTERVAL/60)+":"+Math.round(value/INTERVAL)+"."+(value % INTERVAL);
 		return (
 			<div class="container-fluid align-items-center">
 				<h1 class="display-1">Таймер</h1>
@@ -33,10 +47,10 @@ class App extends Component {
 					<span><kbd>{Math.floor(value/INTERVAL/60) % 60}</kbd> : </span>
 					<span><kbd>{Math.floor(value/INTERVAL) % 60}</kbd> . </span>
 					<span><kbd>{value % INTERVAL < 10 ? '0' : ''}{value % INTERVAL}</kbd></span>
-				</h1>						
+				</h1>		
 				<div>
-					<button class="display-4">Остановить</button>&nbsp;
-					<button class="display-4">Сбросить</button>
+					<button class="display-4" onClick={this.stopTimer}>Остановить</button>&nbsp;
+					<button class="display-4" onClick={this.resetTimer}>Сбросить</button>
 				</div>
 			</div>
 		);
