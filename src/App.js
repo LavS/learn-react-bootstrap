bedabf3c5cd96c25f2be59ce21ee820506a1a7e4
Lavsexpert <lavrov-sergey@yandex.ru>
2018-12-02 09:27:07 +0300

    Более сложное приложение Таймер, на примере которого изучаются конструктор, методы жизненного цикла и изменение состояния.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,41 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component {
-  render() {
-    return <h1>Привет, {this.props.name}!</h1>;
-  }
+const INTERVAL = 100;
+
+class App extends Component {
+	
+    constructor(props) {
+		super(props);
+		this.state = {value: 0};
+    }
+
+    increment(){
+		this.setState({value: this.state.value + 1});
+    }
+
+    componentDidMount() {
+		this.timerID = setInterval(() => this.increment(), 1000/INTERVAL);
+    }
+
+    componentWillUnmount() {
+		clearInterval(this.timerID);
+    }
+
+    render() {
+		const value = this.state.value
+		return (
+			<div>
+				<p>Таймер:</p>
+				<p>
+					<span>{Math.floor(value/INTERVAL/60/60)} : </span>
+					<span>{Math.floor(value/INTERVAL/60) % 60} : </span>
+					<span>{Math.floor(value/INTERVAL) % 60} . </span>
+					<span>{value % INTERVAL}</span>
+				</p>
+			</div>
+		);
+    }
 }
 
 export default App;
