a80f0108f5491a208f28b74178e4b22f9913b40b
Lavsexpert <lavrov-sergey@yandex.ru>
2018-12-02 20:54:10 +0300

    Теперь кнопка "Остановить" может работать в 2х режимах: Остановить и Продолжить.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const INTERVAL = 100;
@@ -28,7 +27,14 @@ class App extends Component {
     }
 	
 	stopTimer(){
-		clearInterval(this.timerID);
+		this.setState({stopped: !this.state.stopped});
+		if(this.state.stopped){
+			clearInterval(this.timerID);
+		}
+		else
+		{
+			this.timerID = setInterval(() => this.increment(), 1000/INTERVAL);
+		};
 	}
 
 	resetTimer(){
@@ -37,20 +43,18 @@ class App extends Component {
 
     render() {
 		const value = this.state.value;
-		document.title = "Таймер: "+Math.floor(value/INTERVAL/60/60)+":"
-		+Math.round(value/INTERVAL/60)+":"+Math.round(value/INTERVAL)+"."+(value % INTERVAL);
 		return (
-			<div class="container-fluid align-items-center">
-				<h1 class="display-1">Таймер</h1>
-				<h1 class="display-1">
+			<div className="container-fluid align-items-center">
+				<h1 className="display-1">Таймер</h1>
+				<h1 className="display-1">
 					<span><kbd>{Math.floor(value/INTERVAL/60/60)}</kbd> : </span>
 					<span><kbd>{Math.floor(value/INTERVAL/60) % 60}</kbd> : </span>
 					<span><kbd>{Math.floor(value/INTERVAL) % 60}</kbd> . </span>
 					<span><kbd>{value % INTERVAL < 10 ? '0' : ''}{value % INTERVAL}</kbd></span>
 				</h1>		
 				<div>
-					<button class="display-4" onClick={this.stopTimer}>Остановить</button>&nbsp;
-					<button class="display-4" onClick={this.resetTimer}>Сбросить</button>
+					<button className="display-4" onClick={this.stopTimer}>{this.state.stopped?'Продолжить':'Остановить'}</button>&nbsp;
+					<button className="display-4" onClick={this.resetTimer}>Сбросить</button>
 				</div>
 			</div>
 		);
